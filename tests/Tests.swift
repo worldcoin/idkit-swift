d8ffa9325b5b90d2f08d25207c4ac5a9ac7c9114
ukit-tokyo <t.yuki@ukit.tokyo>
2024-07-16 17:10:25 +0900

    Update support Swift5.x

--- tests/Tests.swift
+++ tests/Tests.swift
@@ -1,24 +1,28 @@
-@testable import IDKit
-import Testing
+/*
+	Uncomment out after upgrading to Swift 6+
+*/
 
-@Test(.disabled("Run this manually to test the library!")) func testFlow() async throws {
-	let session = try await Session(AppID("app_ce4cb73cb75fc3b73b71ffb4de178410"), action: "test-action")
+// @testable import IDKit
+// import Testing
 
-	let connect_url = session.connect_url
+// @Test(.disabled("Run this manually to test the library!")) func testFlow() async throws {
+// 	let session = try await Session(AppID("app_ce4cb73cb75fc3b73b71ffb4de178410"), action: "test-action")
 
-	// Generate a QR Code with this URL and scan it with World App
-	print("Connection URL: \(connect_url)")
+// 	let connect_url = session.connect_url
 
-	for try await status in session.status() {
-		switch status {
-			case .waitingForConnection:
-				print("Waiting for the user to scan the QR Code")
-			case .awaitingConfirmation:
-				print("Awaiting user confirmation")
-			case let .confirmed(proof):
-				print("Got proof: \(proof)")
-			case let .failed(error):
-				print("Got error: \(error.localizedDescription)")
-		}
-	}
-}
+// 	// Generate a QR Code with this URL and scan it with World App
+// 	print("Connection URL: \(connect_url)")
+
+// 	for try await status in session.status() {
+// 		switch status {
+// 			case .waitingForConnection:
+// 				print("Waiting for the user to scan the QR Code")
+// 			case .awaitingConfirmation:
+// 				print("Awaiting user confirmation")
+// 			case let .confirmed(proof):
+// 				print("Got proof: \(proof)")
+// 			case let .failed(error):
+// 				print("Got error: \(error.localizedDescription)")
+// 		}
+// 	}
+// }
